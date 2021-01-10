fc571b8d0cf5409f937ee9a339a79915dacab9d4
Sadiq <salmansoor@localhost.localdomain>
2021-01-10 15:19:22 +0300

    Solved the Conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
 // Define the numbers and the operation here
+const firstNumber = 10;
+const secondNumber = 5;
+const operation = "+";
 
 // Write if-statements here
+
+if (operation === "+") {
+  const result = firstNumber + secondNumber;
+  console.log(`The result of the operation is ${result} `);
+}
